784e798a99d7b7f67be92f4553cba0164121a6b2
cjilck <99537625+cjilck@users.noreply.github.com>
2022-02-12 15:09:28 +0300

    Update and rename sbot aplha(nihuya ne rabotaet).py to writer.py

--- writer.py
+++ writer.py
@@ -1,11 +1,16 @@
 from pynput import mouse
 import time
 def on_click(x, y, button, pressed):
+    f = open('script.txt','r+')
+    old = f.read()
     print('{0} at {1}'.format(
         'Pressed' if pressed else 'Released',
         (time.perf_counter())))
     s = time.perf_counter()
     g = str(s)
+    old = str(old)
+    s = str(s)
+    f.write(s+'\n')
         # Stop listener
     if not pressed:
         return on_click
